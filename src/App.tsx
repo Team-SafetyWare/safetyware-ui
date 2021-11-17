2fc376383c380ba1d02bfe4b69657dbb7e8928a4
Melissa <57367085+melpicazo@users.noreply.github.com>
2021-11-16 19:11:56 -0700

    configure some basic router stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+          {/* once login is complete, must add component={Login} to following route*/}
+          <Route exact path="/login"/>
+          <Route exact path="/" component={Home}>
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
