650055229dceebe330725565bf952fe6bf7406bb
Melissa <57367085+melpicazo@users.noreply.github.com>
2021-11-16 19:38:37 -0700

    add a button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 const Home = () => (
   <div>
     <h1>Home</h1>
+    <Button variant="contained">wassup</Button>
   </div>
 );
 
