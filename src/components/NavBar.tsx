cc610ccd1f8a058985393ffb544605db785ec34f
Melissa <57367085+melpicazo@users.noreply.github.com>
2021-11-18 15:17:35 -0700

    SAF-38
    - added @mui/styles
    - fixed jsx files into tsx
    - added some custom styling for a button in home
    - changed export method for components

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const SideNav: React.FC = () => {
+    return (
+        <div>
+            <h1>hello this is sidenav but we are not using it yet</h1>
+        </div>
+    )
+}
+  
