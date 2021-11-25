977c5944057a2bdccbacf9d687f2f2e5104b8946
Rainer Lim <limrainer.ph@gmail.com>
2021-11-25 13:26:49 -0700

    SAF-66: Env: Add auto-formatting settings
    For VS Code workspaces, created a settings.json file to include
    auto-formatting settings, as well as sorting of imports in JS files.
    
    Additionally, created an extensions.json file to include recommended
    extensions for VS Code.
    
    Reformatted all files in src directory according to abovementioned
    settings except for logo.svg.
    
    Integration Instructions:
    1. Install the recommended VS Code extensions by going to the
       "Extensions" tab, searching for "@recommended:workspace", and
       installing the resulting list of extensions.
    2. On save, imports are sorted and modified lines of code are
       auto-formatted.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 
 export const SideNav: React.FC = () => {
-    return (
-        <div>
-            <h1>hello this is sidenav but we are not using it yet</h1>
-        </div>
-    )
-}
-  
+  return (
+    <div>
+      <h1>hello this is sidenav but we are not using it yet</h1>
+    </div>
+  );
+};
