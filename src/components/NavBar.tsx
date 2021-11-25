bca9c3c211715e4aca54a29bd7b59446e9c9bb06
Melissa <57367085+melpicazo@users.noreply.github.com>
2021-11-25 16:19:54 -0700

    SAF-19
    joel navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
+import { styled } from '@mui/system';
+import React from "react";
 
-export const SideNav: React.FC = () => {
+const useStyles = makeStyles(() => ({
+    header: {
+        backgroundColor: "#ab3535",
+    },
+    logo: {
+        fontFamily: "Myriad",
+        fontWeight: 700,
+        color: "#fff000",
+        textAlign: "left",
+    },
+}));
+
+
+export const NavBar: React.FC = () => {
+    const { header } = useStyles();
+    const displayDesktop = () => {
+        return <Toolbar>Blackline Safety</Toolbar>;
+    };
+
+    const logo = (
+        <Typography variant="h6" component="h1">
+            Blackline Safety
+        </Typography>
+    );
+
     return (
-        <div>
-            <h1>hello this is sidenav but we are not using it yet</h1>
-        </div>
-    )
+        <header>
+            <AppBar className={header}>{displayDesktop()}</AppBar>
+        </header>
+    );
 }
-  
+
+
+
