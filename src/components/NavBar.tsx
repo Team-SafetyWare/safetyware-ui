362c6005d85fe8cc38ac08da2dec7643e8412433
Joel Poirier <rushfan347@gmail.com>
2022-01-04 14:12:10 -0700

    Updated NavBar
    Added logo and menu elements to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,66 @@
-import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
+import { AppBar, Toolbar, Button, Typography, makeStyles } from "@material-ui/core";
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import logo from "../assets/logo.png"
+
+const headersData = [
+    {
+      label: "My Account",
+      href: "/account",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ];
 
 const useStyles = makeStyles(() => ({
     header: {
-        backgroundColor: "#ab3535",
+        backgroundColor: "#ffffff",
     },
-    logo: {
-        fontFamily: "Myriad",
-        fontWeight: 700,
-        color: "#fff000",
-        textAlign: "left",
+    menuButton: {
+        color: "#d34949",
+        marginLeft:"auto",
+        marginRight:"30px",
+        fontSize:"14px"
     },
+    toolbar: {
+        display: "flex",
+        justifyContent: "space-between",
+      },
 }));
+
+const blacklineLogo = (
+<img src={logo} alt="Blackline Safety" height="60" width="225" />
+)
 
 
 export const NavBar: React.FC = () => {
-    const { header } = useStyles();
+    const { header, menuButton, toolbar } = useStyles();
     const displayDesktop = () => {
-        return <Toolbar>Blackline Safety</Toolbar>;
+        return <Toolbar className={toolbar}>
+            {blacklineLogo}
+            <div>{getMenuButtons()}</div>
+            </Toolbar>;
     };
 
-    const logo = (
-        <Typography variant="h6" component="h1">
-            Blackline Safety
-        </Typography>
-    );
+    const getMenuButtons = () => {
+        return headersData.map(({ label, href }) => {
+          return (
+            <Button
+              {...{
+                key: label,
+                color: "inherit",
+                to: href,
+                component: RouterLink,
+                className: menuButton
+              }}
+            >
+              {label}
+            </Button>
+          );
+        });
+      };
 
     return (
         <header>
@@ -32,6 +68,4 @@ export const NavBar: React.FC = () => {
         </header>
     );
 }
-
-
 
