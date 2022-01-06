4424236e0cd794d3153926490ede72d729d6ce55
nmoton <26640786+nmoton@users.noreply.github.com>
2022-01-06 09:50:27 -0700

    SAF-18: Fixed small styling bug

--- src/components/UI/atoms/TeamSelect.tsx
+++ src/components/UI/atoms/TeamSelect.tsx
@@ -1,12 +1,33 @@
 import FormControl from "@mui/material/FormControl";
+import InputBase from "@mui/material/InputBase";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
+import { styled } from "@mui/material/styles";
 import { makeStyles } from "@mui/styles";
 import React from "react";
 
 interface TeamSelectProps {}
 
-const useStyles = makeStyles({});
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+  "& .MuiSelect-select": {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    fontWeight: "bold",
+  },
+}));
+
+const useStyles = makeStyles({ selectBar: { fontWeight: "bold" } });
 
 export const TeamSelect: React.FC<TeamSelectProps> = (props) => {
   const styles = useStyles();
@@ -15,6 +36,7 @@ export const TeamSelect: React.FC<TeamSelectProps> = (props) => {
     <FormControl variant="standard" sx={{ minWidth: 120 }}>
       <Select
         defaultValue={"123-ABC-456"}
+        input={<BootstrapInput />}
         disableUnderline
         //   value={team}
         //   onChange={handleChange}
