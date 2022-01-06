c845387b7e2ea3a3a41c38fac9cc778d0f8625bb
nmoton <26640786+nmoton@users.noreply.github.com>
2022-01-06 07:26:30 -0700

    SAF-18: Added table with mock data

--- src/components/UI/atoms/TeamInfo.tsx
+++ src/components/UI/atoms/TeamInfo.tsx
@@ -7,31 +7,67 @@ import React from "react";
 interface TeamInfoProps {}
 
 const useStyles = makeStyles({
-  userInfo: {
-    display: "flex",
-    alignItems: "center",
-  },
   teamSelect: {},
+  teamTable: {
+    border: "1px solid black",
+    width: "100%",
+    borderCollapse: "collapse",
+    textAlign: "center",
+  },
+  tableHeader: {
+    border: "1px solid black",
+  },
+  tableRow: {
+    border: "1px solid black",
+  },
+  tableColumn: {
+    border: "1px solid black",
+    padding: "10px",
+  },
 });
 
 export const TeamInfo: React.FC<TeamInfoProps> = (props) => {
   const styles = useStyles();
 
   return (
-    <div>
-      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-        <Select
-          className={styles.teamSelect}
-          id="demo-simple-select-standard"
-          defaultValue={10}
-          disableUnderline
-          //   value={age}
-          //   onChange={handleChange}
-        >
-          <MenuItem value={10}>Team 123-ABC-456</MenuItem>
-          <MenuItem value={20}>Team 987-ZYX-321</MenuItem>
-        </Select>
-      </FormControl>
-    </div>
+    <>
+      <div className={styles.teamSelect}>
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+          <Select
+            defaultValue={10}
+            disableUnderline
+            //   value={team}
+            //   onChange={handleChange}
+          >
+            <MenuItem value={10}>Team 123-ABC-456</MenuItem>
+            <MenuItem value={20}>Team 987-ZYX-321</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div>
+        <table className={styles.teamTable}>
+          <tr className={styles.tableHeader}>
+            <td className={styles.tableColumn}>Member</td>
+            <td className={styles.tableColumn}>Email</td>
+            <td className={styles.tableColumn}>Role</td>
+          </tr>
+          <tr className={styles.tableRow}>
+            <td className={styles.tableColumn}>Jane Doe (You)</td>
+            <td className={styles.tableColumn}>jane.doe@blackline.ca</td>
+            <td className={styles.tableColumn}>Manager</td>
+          </tr>
+          <tr className={styles.tableRow}>
+            <td className={styles.tableColumn}>Jeff Davids</td>
+            <td className={styles.tableColumn}>jeff.davids@blackline.ca</td>
+            <td className={styles.tableColumn}>Member</td>
+          </tr>
+          <tr className={styles.tableRow}>
+            <td className={styles.tableColumn}>John Hanley</td>
+            <td className={styles.tableColumn}>john.hanley@blackline.ca</td>
+            <td className={styles.tableColumn}>Member</td>
+          </tr>
+        </table>
+      </div>
+    </>
   );
 };
