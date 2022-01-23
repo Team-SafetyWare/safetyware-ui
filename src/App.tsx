0edeb29f19866c0bea41b8a9aab0b914aa05815a
Rainer Lim <limrainer.ph@gmail.com>
2022-01-23 13:50:23 -0700

    SAF-111: Static: Move pages to its own component
    Moved all pages to its own component. By doing so, the useLocation hook
    is available in the new Routes component. With this hook, the Pages
    component is able to conditionally render either the Login page (i.e., a
    page without a sidebar) or any other page based on the URL.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { makeStyles } from "@mui/styles";
 import React from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import ProfilePicture from "./assets/profilePicture.png";
-import { Page } from "./components/UI/atoms/Page";
-import { Sidebar } from "./components/UI/molecules/Sidebar";
-import { Gases } from "./components/UI/organisms/Gases";
-import { Home } from "./components/UI/organisms/Home";
-import { Incidents } from "./components/UI/organisms/Incidents";
-import { Locations } from "./components/UI/organisms/Locations";
-import { Login } from "./components/UI/organisms/Login";
-import { UserAccount } from "./components/UI/organisms/UserAccount";
+import { Pages } from "./components/Pages";
 
 const theme = createTheme({
   // Use the system font instead of the default Roboto font.
@@ -31,67 +22,11 @@ const theme = createTheme({
   },
 });
 
-const useStyles = makeStyles({
-  content: {
-    height: "100vh",
-    width: "100vw",
-    paddingLeft: "240px",
-  },
-  innerContent: {
-    height: "100%",
-    width: "100%",
-    padding: "10px 25px",
-  },
-});
-
 function App() {
-  const styles = useStyles();
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Sidebar userName="Jane Doe" userPhoto={ProfilePicture} />
-        <div className={styles.content}>
-          <div className={styles.innerContent}>
-            <Switch>
-              <Page
-                exact
-                path="/login"
-                title="Blackline Safety | Login"
-                component={Login}
-              />
-              <Page
-                exact
-                path="/"
-                title="Blackline Safety | Home"
-                component={Home}
-              />
-              <Page
-                exact
-                path="/locations"
-                title="Blackline Safety | Locations"
-                component={Locations}
-              />
-              <Page
-                exact
-                path="/incidents"
-                title="Blackline Safety | Incidents"
-                component={Incidents}
-              />
-              <Page
-                exact
-                path="/gases"
-                title="Blackline Safety | Gases"
-                component={Gases}
-              />
-              <Page
-                exact
-                path="/user-account"
-                title="Blackline Safety | My Account"
-                component={UserAccount}
-              />
-            </Switch>
-          </div>
-        </div>
+        <Pages />
       </Router>
     </ThemeProvider>
   );
