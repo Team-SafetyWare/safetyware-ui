a3b9c6999965f46edf9765e413135b569b6123c1
nmoton <26640786+nmoton@users.noreply.github.com>
2022-01-26 22:59:29 -0700

    SAF-137: Initial commit for react-beautiful-dnd

--- src/components/UI/molecules/DashboardSummary.tsx
+++ src/components/UI/molecules/DashboardSummary.tsx
@@ -2,13 +2,13 @@ import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
 import BubbleChartOutlinedIcon from "@mui/icons-material/BubbleChartOutlined";
 import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
 import { makeStyles } from "@mui/styles";
-import React from "react";
-import { DashboardSummaryTile } from "../atoms/DashboardSummaryTile";
+import React, { useState } from "react";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import { HorizontalColumn } from "../atoms/HorizontalColumn";
 
 const useStyles = makeStyles({
   pageInfo: {
     width: "100%",
-    display: "flex",
   },
   summaryFirst: {
     width: "33%",
@@ -25,36 +25,72 @@ const useStyles = makeStyles({
   },
 });
 
-export const DashboardSummary: React.FC = () => {
-  const styles = useStyles();
+interface Item {
+  id: string;
+  content: string;
+  name: string;
+  number: string;
+  summaryTileIcon: any;
+}
 
+// a little function to help us with reordering the result
+const reorder = (
+  list: Item[],
+  startIndex: number,
+  endIndex: number
+): Item[] => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+export const DashboardSummary = (): JSX.Element => {
+  const [state, setState] = useState([
+    {
+      id: "1",
+      content: "hello",
+      name: "New Location Updates",
+      number: "2",
+      summaryTileIcon: <ExploreOutlinedIcon style={{ fontSize: 42 }} />,
+    },
+    {
+      id: "2",
+      content: "hi",
+      name: "New Incidents",
+      number: "3",
+      summaryTileIcon: <BarChartOutlinedIcon style={{ fontSize: 42 }} />,
+    },
+    {
+      id: "3",
+      content: "sup",
+      name: "New Gas Readings",
+      number: "0",
+      summaryTileIcon: <BubbleChartOutlinedIcon style={{ fontSize: 42 }} />,
+    },
+  ]);
+
+  const onDragEnd = (result: DropResult): void => {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items: Item[] = reorder(
+      state,
+      result.source.index,
+      result.destination.index
+    );
+
+    setState(items);
+  };
+
+  // Normally you would want to split things out into separate components.
+  // But in this example everything is just done in one place for simplicity
   return (
-    <>
-      <div className={styles.pageInfo}>
-        <div className={styles.summaryFirst}>
-          <DashboardSummaryTile
-            summaryTileIcon={<ExploreOutlinedIcon style={{ fontSize: 42 }} />}
-            summaryName={"New Location Updates"}
-            summaryNumber={"2"}
-          />
-        </div>
-        <div className={styles.summarySecond}>
-          <DashboardSummaryTile
-            summaryTileIcon={<BarChartOutlinedIcon style={{ fontSize: 42 }} />}
-            summaryName={"New Incidents"}
-            summaryNumber={"3"}
-          />
-        </div>
-        <div className={styles.summarySecond}>
-          <DashboardSummaryTile
-            summaryTileIcon={
-              <BubbleChartOutlinedIcon style={{ fontSize: 42 }} />
-            }
-            summaryName={"New Gas Readings"}
-            summaryNumber={"0"}
-          />
-        </div>
-      </div>
-    </>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <HorizontalColumn state={state} />
+    </DragDropContext>
   );
 };
