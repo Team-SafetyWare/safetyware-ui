18aaa3e3ea8782c40d1a84b3943e883bf35cecf9
Kakkarrot <lqf5474@gmail.com>
2022-01-27 01:30:42 -0700

    SAF-118 added marker windows to accident dot map

--- src/components/UI/atoms/AccidentDotMap.tsx
+++ src/components/UI/atoms/AccidentDotMap.tsx
@@ -4,50 +4,47 @@ import {setFlagsFromString} from "v8";
 import {LocationReading} from '../organisms/Locations';
 
 const containerStyle = {
-  width: '100%',
-  height: '100%',
+    width: '100%',
+    height: '100%',
 };
 
 interface AccidentDotMapProps {
-  accidents?: any;
-  startDate?: any;
-  endDate?: any;
-  center?: any;
-  zoom?: any;
+    accidents?: any;
+    startDate?: any;
+    endDate?: any;
+    center?: any;
+    zoom?: any;
 }
 
 export const AccidentDotMap: React.FC<AccidentDotMapProps> = (props) => {
-  const accidents = props.accidents
-  const zoom = props.zoom
-  const center = props.center
-  const [markerWindowLat, updateWindowLat] = React.useState(51.049999)
-  const [markerWindowLng, updateWindowLng] = React.useState(-114.1283)
-  const [markerWindowText, updateWindowText] = React.useState("Test")
-  // const [markerWindows, updateMarkerWindows] = React.useState([])
+    const accidents = props.accidents
+    const zoom = props.zoom
+    const center = props.center
+    const [markerWindows, addMarkerWindow] = React.useState<LocationReading[]>([]);
 
-  const handleHover = (location: any) => {
-    console.log("nothing")
-  }
+    function createMarker(location: LocationReading) {
+        return <Marker position={location.coordinates} onClick={() => {
+            addMarkerWindow(markerWindows => [...markerWindows, location])
+        }}/>;
+    }
 
-
-  function createMarker(location: LocationReading) {
-    return <Marker position={location.coordinates}/>;
-  }
-
-  function createMarkerWindow(location: any) {
-
-  }
+    function createMarkerWindow(location: LocationReading) {
+        return <InfoWindow position={location.coordinates}>
+            <div>{location.name}</div>
+        </InfoWindow>
+    }
 
 
-  return (
+    return (
         <GoogleMap
             mapContainerStyle={containerStyle}
             center={center}
             zoom={zoom}
         >
-          {accidents.map((accident: LocationReading) => createMarker(accident))}
+            {markerWindows.map((markerWindow) => createMarkerWindow(markerWindow))}
+            {accidents.map((accident: LocationReading) => createMarker(accident))}
         </GoogleMap>
-  )
+    )
 }
 
 export default React.memo(AccidentDotMap)
