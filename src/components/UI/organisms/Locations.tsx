4ad8011e3e8c2456f629960596b72e084996e321
Joel Poirier <rushfan347@gmail.com>
2022-01-27 16:06:48 -0700

    Added Reduced CustomBox for Travel Trail

--- src/components/UI/organisms/Locations.tsx
+++ src/components/UI/organisms/Locations.tsx
@@ -1,41 +1,50 @@
-import React, {useEffect} from "react";
-import {CustomAccordion} from "../atoms/CustomAccordion";
-import {HazardousAreaHeatMap} from "../atoms/HazardousAreaHeatMap";
-import {TravelHistoryTrail} from "../atoms/TravelHistoryTrail";
+import { useQuery } from "@apollo/client";
+import React, { useEffect } from "react";
+import { GET_LOCATIONS } from "../../../util/queryService";
+import { CustomAccordion } from "../atoms/CustomAccordion";
 import CustomCollapsibleTable from "../atoms/CustomCollapsibleTable";
+import { HazardousAreaHeatMap } from "../atoms/HazardousAreaHeatMap";
 import { PageHeader } from "../atoms/PageHeader";
-import {PageSectionHeader} from "../atoms/PageSectionHeader";
-import {useQuery} from "@apollo/client";
-import {GET_LOCATIONS} from "../../../util/queryService";
-import {LocationReading} from "./Incidents";
+import { PageSectionHeader } from "../atoms/PageSectionHeader";
+import { TravelHistoryTrail } from "../atoms/TravelHistoryTrail";
+import { CustomBoxReduced } from "../molecules/CustomBoxReduced";
+import { LocationReading } from "./Incidents";
 
 const center = {
-    lat: 51.049999,
-    lng: -114.1283,
+  lat: 51.049999,
+  lng: -114.1283,
 };
 
 const path = [
-    {lat: 51.077763, lng: -114.140657},
-    {lat: 51.046048773481786, lng: -114.02334120770176},
+  { lat: 51.077763, lng: -114.140657 },
+  { lat: 51.046048773481786, lng: -114.02334120770176 },
 ];
 
-export const Locations: React.FC = () => {
-    const [locations, addLocation] = React.useState<LocationReading[]>([]);
-    const {loading, error, data} = useQuery(
-        GET_LOCATIONS,
-    );
+const user = "PersonA";
+const view = "User";
+const incidentType = "All";
+const startDate = new Date("01/01/2022");
+const endDate = new Date("01/08/2022");
 
-    useEffect(() => {
-        if (!loading && data) {
-            data.locationReadings.map(
-                (location: any) => {
-                    addLocation(locations => [...locations,
-                        {coordinates: {lng: location.coordinates[0], lat: location.coordinates[1]}}
-                    ])
-                }
-            )
-        }
-    }, [loading, data])
+export const Locations: React.FC = () => {
+  const [locations, addLocation] = React.useState<LocationReading[]>([]);
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  useEffect(() => {
+    if (!loading && data) {
+      data.locationReadings.map((location: any) => {
+        addLocation((locations) => [
+          ...locations,
+          {
+            coordinates: {
+              lng: location.coordinates[0],
+              lat: location.coordinates[1],
+            },
+          },
+        ]);
+      });
+    }
+  }, [loading, data]);
 
   return (
     <>
@@ -70,8 +79,18 @@ export const Locations: React.FC = () => {
         accordionWidth={""}
         accordionTitle={"Hazardous Area Heat Map"}
         component={
-          <HazardousAreaHeatMap accidents={locations} center={center} zoom={10} />
+          <HazardousAreaHeatMap
+            accidents={locations}
+            center={center}
+            zoom={10}
+          />
         }
+      />
+      <CustomBoxReduced
+        user={user}
+        view={view}
+        startDate={startDate}
+        endDate={endDate}
       />
     </>
   );
