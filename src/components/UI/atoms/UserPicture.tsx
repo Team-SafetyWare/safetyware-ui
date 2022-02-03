b9cb562b77b937754503f3552452e6716542b328
Rainer Lim <limrainer.ph@gmail.com>
2022-02-03 06:35:46 -0700

    Clean up theme and make account page responsive
    Created a separate Theme.tsx file so that any file can import and access
    the global theme.
    
    Made User Account page responsive vertically.

--- src/components/UI/atoms/UserPicture.tsx
+++ src/components/UI/atoms/UserPicture.tsx
@@ -1,6 +1,7 @@
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import { makeStyles } from "@mui/styles";
 import React from "react";
+import theme from "../../../Theme";
 
 interface UserPictureProps {
   userPhoto?: string;
@@ -12,6 +13,9 @@ const useStyles = makeStyles({
     display: "flex",
     alignItems: "center",
     margin: "0",
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: "20px",
+    },
   },
   editText: {
     height: "100%",
