e304c0843f932653af5bd1c1343d915cdcaf0c0f
Rainer Lim <limrainer.ph@gmail.com>
2022-02-10 16:54:06 -0700

    Make Gases page responsive
    Made Gases page responsive in portrait mode.

--- src/components/UI/organisms/Gases.tsx
+++ src/components/UI/organisms/Gases.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import { useMediaQuery } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import React, { useState } from "react";
+import theme from "../../../Theme";
 import { CustomAccordion } from "../atoms/CustomAccordion";
 import CustomCollapsibleTable from "../atoms/CustomCollapsibleTable";
 import { GasesDotMap } from "../atoms/GasesDotMap";
 import { PageHeader } from "../atoms/PageHeader";
 import { PageSectionHeader } from "../atoms/PageSectionHeader";
+import { VisualizationSelect } from "../atoms/VisualizationSelect";
 
 const gases = [
   { lat: 51.077763, lng: -114.140657 },
@@ -15,35 +19,89 @@ const center = {
   lng: -114.1283,
 };
 
+const useStyles = makeStyles({
+  gasesDropdown: {
+    [theme.breakpoints.down("sm")]: {
+      display: "flex",
+      justifyContent: "center",
+      left: "50%",
+      marginBottom: "20px",
+      position: "absolute",
+      top: "calc(0.5 * 60px)",
+      transform: "translate(-50%, -50%)",
+    },
+  },
+
+  visualization: {
+    [theme.breakpoints.down("sm")]: {
+      height: "calc(100vh - 60px)",
+      left: "0",
+      position: "absolute",
+      top: "60px",
+      width: "100vw",
+    },
+  },
+});
+
 export const Gases: React.FC = () => {
+  const matches = useMediaQuery("(min-width:600px)");
+  const styles = useStyles();
+
+  const visualizations = ["Raw Gases Data Table", "Gases Dot Map"];
+
+  const [visualization, setVisualization] = useState(visualizations[0]);
+
   return (
     <>
-      <PageHeader
-        pageTitle={"Gases"}
-        pageDescription={"Description of the Gases Page and What it Does"}
-      />
-      <PageSectionHeader
-        sectionTitle={"Raw Gases Data"}
-        sectionDescription={"Explore and Download Raw Gases Data"}
-        download={true}
-      />
-      <CustomAccordion
-        accordionHeight={"auto"}
-        accordionWidth={""}
-        accordionTitle={"Raw Gases Data Table"}
-        component={<CustomCollapsibleTable />}
-      />
-      <PageSectionHeader
-        sectionTitle={"Gas Visualizations"}
-        sectionDescription={"Visualize Gas Data Based on Location"}
-        download={false}
-      />
-      <CustomAccordion
-        accordionHeight={"400px"}
-        accordionWidth={""}
-        accordionTitle={"Gases"}
-        component={<GasesDotMap gases={gases} center={center} zoom={10} />}
-      />
+      {matches ? (
+        <>
+          <PageHeader
+            pageTitle={"Gases"}
+            pageDescription={"Description of the Gases Page and What it Does"}
+          />
+          <PageSectionHeader
+            sectionTitle={"Raw Gases Data"}
+            sectionDescription={"Explore and Download Raw Gases Data"}
+            download={true}
+          />
+          <CustomAccordion
+            accordionHeight={"auto"}
+            accordionWidth={""}
+            accordionTitle={visualizations[0]}
+            component={<CustomCollapsibleTable />}
+          />
+          <PageSectionHeader
+            sectionTitle={"Gas Visualizations"}
+            sectionDescription={"Visualize Gas Data Based on Location"}
+            download={false}
+          />
+          <CustomAccordion
+            accordionHeight={"400px"}
+            accordionWidth={""}
+            accordionTitle={visualizations[1]}
+            component={<GasesDotMap gases={gases} center={center} zoom={10} />}
+          />
+        </>
+      ) : (
+        <>
+          <div className={styles.gasesDropdown}>
+            <VisualizationSelect
+              visualizations={visualizations}
+              setVisualization={setVisualization}
+            />
+          </div>
+          {visualization == visualizations[0] && (
+            <div className={styles.visualization}>
+              <CustomCollapsibleTable />
+            </div>
+          )}
+          {visualization == visualizations[1] && (
+            <div className={styles.visualization}>
+              <GasesDotMap gases={gases} center={center} zoom={10} />
+            </div>
+          )}
+        </>
+      )}
     </>
   );
 };
