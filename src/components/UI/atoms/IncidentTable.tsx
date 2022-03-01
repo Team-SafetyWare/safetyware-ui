347f91dff3058a9b5c7d23a4b3cc3d45445e976d
Rainer Lim <limrainer.ph@gmail.com>
2022-03-01 01:28:06 -0700

    Enable filtering by date on Incidents table
    Applied the same filter-by-date mechanism of the Incidents dot map on
    the Incidents table.

--- src/components/UI/atoms/IncidentTable.tsx
+++ src/components/UI/atoms/IncidentTable.tsx
@@ -5,7 +5,12 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import * as React from "react";
+import { Fragment, useEffect, useState } from "react";
+import {
+  selectIncidentPageEndDate,
+  selectIncidentPageStartDate,
+} from "../../../store/slices/incidentPageSlice";
+import { useAppSelector } from "../../../store/store";
 import { IncidentReadings } from "../organisms/Incidents";
 
 interface IncidentTableProps {
@@ -14,10 +19,9 @@ interface IncidentTableProps {
 
 function Row(props: { row: IncidentReadings }) {
   const { row } = props;
-  const [open, setOpen] = React.useState(false);
 
   return (
-    <React.Fragment>
+    <Fragment>
       <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
         <TableCell>{row.type}</TableCell>
         <TableCell>{row.personName}</TableCell>
@@ -26,14 +30,63 @@ function Row(props: { row: IncidentReadings }) {
           {row.coordinates.lng.toFixed(5)}, {row.coordinates.lat.toFixed(5)}
         </TableCell>
       </TableRow>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
 export default function IncidentTable(props: IncidentTableProps) {
+  const [incidents, updateIncidents] = useState<IncidentReadings[]>([]);
+  const [filteredIncidents, updateFilteredIncidents] = useState<
+    IncidentReadings[]
+  >([]);
+  const startDate = useAppSelector(selectIncidentPageStartDate);
+  const endDate = useAppSelector(selectIncidentPageEndDate);
+
+  function inDateRange(date: Date, start: Date, end: Date): boolean {
+    return !(
+      date.getTime() < start.getTime() || date.getTime() > end.getTime()
+    );
+  }
+
+  useEffect(() => {
+    updateIncidents(() => props.incidents);
+  }, [props]);
+
+  function createIncident(incident: any) {
+    return {
+      coordinates: {
+        lat: incident.coordinates.lat,
+        lng: incident.coordinates.lng,
+      },
+      timestamp: incident.timestamp,
+      personName: incident.personName,
+      companyName: incident.companyName,
+      type: incident.type,
+    };
+  }
+
+  useEffect(() => {
+    updateFilteredIncidents([]);
+    incidents.map((incident: any) => {
+      if (
+        !inDateRange(
+          new Date(incident.timestamp),
+          new Date(startDate),
+          new Date(endDate)
+        )
+      ) {
+        return;
+      }
+      updateFilteredIncidents((filteredIncidents) => [
+        ...filteredIncidents,
+        createIncident(incident),
+      ]);
+    });
+  }, [incidents, startDate, endDate]);
+
   const rows: IncidentReadings[] = [];
 
-  props.incidents.map((incidentReadings: IncidentReadings) =>
+  filteredIncidents.map((incidentReadings: IncidentReadings) =>
     rows.push(incidentReadings)
   );
 
