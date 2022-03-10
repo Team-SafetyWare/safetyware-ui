33af1f9f4daaa087cb69f0bc143416dce0eb10e9
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-03-10 00:05:59 -0700

    Filter incident stats to current organization

--- src/util/queryService.ts
+++ src/util/queryService.ts
@@ -57,13 +57,11 @@ export const GET_INCIDENTS = gql`
 `;
 
 export const GET_INCIDENT_STATS = gql`
-  {
-    userAccount(id: "1fh2ktnk5gd01c040d0vbc1z") {
-      company {
-        incidentStats {
-          type
-          count
-        }
+  query ($companyId: ID!) {
+    company(id: $companyId) {
+      incidentStats {
+        type
+        count
       }
     }
   }
