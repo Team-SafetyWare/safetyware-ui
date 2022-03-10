ca59bb857bcafbb643c7b0473cb906abc43162f1
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-03-10 00:07:40 -0700

    Sort incident stats

--- src/components/UI/organisms/Incidents.tsx
+++ src/components/UI/organisms/Incidents.tsx
@@ -118,7 +118,8 @@ export const Incidents: React.FC = () => {
           y: stat.count,
         };
       })
-      .flat() ?? [];
+      .flat()
+      .sort((s1: any, s2: any) => (s1.x > s2.x ? 1 : -1)) ?? [];
 
   const visualizations = [
     "Raw Incidents Data Table",
