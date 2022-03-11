6b6ff4a7f790f29fc5cffa5e07f3418dc7dcfb6c
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-03-11 14:44:08 -0700

    Add incomplete legend to map

--- src/components/UI/atoms/TravelHistoryTrail.tsx
+++ src/components/UI/atoms/TravelHistoryTrail.tsx
@@ -1,5 +1,6 @@
 import { GoogleMap, Polyline } from "@react-google-maps/api";
 import React from "react";
+import ControlPosition = google.maps.ControlPosition;
 
 interface TravelHistoryTrailProps {
   center?: any;
@@ -28,28 +29,38 @@ export const TravelHistoryTrail: React.FC<TravelHistoryTrailProps> = (
   };
 
   return (
-    <GoogleMap
-      mapContainerStyle={mapContainerStyle}
-      zoom={12}
-      center={center}
-      options={{ gestureHandling: "greedy" }}
-    >
-      {segments.map((segment: any) => (
-        // eslint-disable-next-line react/jsx-key
-        <Polyline
-          path={segment.path}
-          options={{
-            strokeColor: segment.color,
-            strokeOpacity: 1,
-            strokeWeight: 3,
-            clickable: false,
-            draggable: false,
-            editable: false,
-            visible: true,
-            zIndex: 1,
-          }}
-        />
-      ))}
-    </GoogleMap>
+    <>
+      <div id={"travel-legend"} style={{ backgroundColor: "red" }}>
+        <h3>Hello. I am the legend.</h3>
+      </div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={12}
+        center={center}
+        options={{ gestureHandling: "greedy" }}
+        onLoad={(map) => {
+          const controls = map.controls[ControlPosition.RIGHT_TOP];
+          const legend = document.getElementById("travel-legend");
+          controls.push(legend);
+        }}
+      >
+        {segments.map((segment: any) => (
+          // eslint-disable-next-line react/jsx-key
+          <Polyline
+            path={segment.path}
+            options={{
+              strokeColor: segment.color,
+              strokeOpacity: 1,
+              strokeWeight: 3,
+              clickable: false,
+              draggable: false,
+              editable: false,
+              visible: true,
+              zIndex: 1,
+            }}
+          />
+        ))}
+      </GoogleMap>
+    </>
   );
 };
