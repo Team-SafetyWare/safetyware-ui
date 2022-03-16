f973739c3b6a4be497992a9ef71a4e90c05b6b95
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-03-15 23:26:10 -0600

    Remove less visible colors

--- src/components/UI/atoms/TravelMap.tsx
+++ src/components/UI/atoms/TravelMap.tsx
@@ -13,24 +13,15 @@ const TRAIL_SPLIT_MS = 10 * 60 * 1000;
 const TRAIL_COLORS = [
   "#e6194b",
   "#3cb44b",
-  "#ffe119",
   "#4363d8",
   "#f58231",
   "#911eb4",
-  "#46f0f0",
   "#f032e6",
-  "#bcf60c",
-  "#fabebe",
   "#008080",
-  "#e6beff",
   "#9a6324",
-  "#fffac8",
   "#800000",
-  "#aaffc3",
   "#808000",
-  "#ffd8b1",
   "#000075",
-  "#808080",
 ];
 
 interface TrailPoint extends LatLngLiteral {
