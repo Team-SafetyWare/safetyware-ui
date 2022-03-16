efdb712b81df6f8be845aaef3a23986db64a08ca
Joel Poirier <rushfan347@gmail.com>
2022-03-16 05:59:27 -0600

    Gas readings on gases dot map added

--- src/components/UI/atoms/GasesDotMap.tsx
+++ src/components/UI/atoms/GasesDotMap.tsx
@@ -1,12 +1,20 @@
-import { GoogleMap, Marker } from "@react-google-maps/api";
-import React from "react";
+import { GoogleMap, InfoWindow, Marker } from "@react-google-maps/api";
+import React, { useEffect } from "react";
+import GenericIcon from "../../../assets/generic.png";
+import {
+  selectGasPageEndDate,
+  selectGasPageName,
+  selectGasPageStartDate,
+} from "../../../store/slices/gasPageSlice";
+import { useAppSelector } from "../../../store/store";
+import { GasReading } from "../organisms/Gases";
 
 const containerStyle = {
   width: "100%",
   height: "100%",
 };
 
-interface GasesDotMapProps {
+interface GasDotMapProps {
   gases?: any;
   startDate?: any;
   endDate?: any;
@@ -14,29 +22,98 @@ interface GasesDotMapProps {
   zoom?: any;
 }
 
-export const GasesDotMap: React.FC<GasesDotMapProps> = (props) => {
-  const gases = props.gases;
+export const GasDotMap: React.FC<GasDotMapProps> = (props) => {
+  const [gases, updateGases] = React.useState<GasReading[]>([]);
   const zoom = props.zoom;
   const center = props.center;
+  const [, updateMarkerWindows] = React.useState<GasReading[]>([]);
+  const [filteredGases, updateFilteredGases] = React.useState<GasReading[]>([]);
+  const startDate = useAppSelector(selectGasPageStartDate);
+  const endDate = useAppSelector(selectGasPageEndDate);
+  const filterName = useAppSelector(selectGasPageName);
+  const [hoverMarker, updateHoverMarker] = React.useState<
+    GasReading | undefined
+  >(undefined);
 
-  function createMarker(
-    location: google.maps.LatLng | google.maps.LatLngLiteral
-  ) {
-    return <Marker position={location} />;
+  function createMarker(gas: GasReading) {
+    const markerIcon = GenericIcon;
+    return (
+      <Marker
+        position={gas.coordinates}
+        icon={markerIcon}
+        onClick={() => {
+          updateMarkerWindows((markerWindows) => [...markerWindows, gas]);
+        }}
+        onMouseOver={() => updateHoverMarker(gas)}
+      />
+    );
   }
 
+  function createHoverWindow(gas?: GasReading) {
+    if (gas) return createMarkerWindow(gas);
+  }
+
+  function createMarkerWindow(gas: GasReading) {
+    return (
+      <InfoWindow
+        position={gas.coordinates}
+        onCloseClick={() => updateHoverMarker(undefined)}
+      >
+        <div>
+          <p>
+            <b>Gas: {gas.gas}</b>
+          </p>
+          <div>Name: {gas.personName}</div>
+          <div>Density: {gas.density + " " + gas.densityUnits}</div>
+          <div>Time: {gas.timestamp?.toLocaleString()}</div>
+        </div>
+      </InfoWindow>
+    );
+  }
+
+  useEffect(() => {
+    updateGases(() => props.gases);
+  }, [props]);
+
+  function createGas(gas: any) {
+    return {
+      coordinates: {
+        lat: gas.coordinates.lat,
+        lng: gas.coordinates.lng,
+      },
+      density: gas.density,
+      densityUnits: gas.densityUnits,
+      gas: gas.gas,
+      timestamp: gas.timestamp,
+      personName: gas.personName,
+    };
+  }
+
+  useEffect(() => {
+    updateFilteredGases([]);
+    updateMarkerWindows([]);
+    updateHoverMarker(undefined);
+    gases.map((gas: any) => {
+      updateFilteredGases((filteredGases) => [
+        ...filteredGases,
+        createGas(gas),
+      ]);
+    });
+  }, [gases, startDate, endDate, filterName]);
+
   return (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={zoom}
-      options={{ gestureHandling: "greedy" }}
-    >
-      {gases.map((accident: google.maps.LatLng | google.maps.LatLngLiteral) =>
-        createMarker(accident)
-      )}
-    </GoogleMap>
+    <>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={zoom}
+        options={{ gestureHandling: "greedy" }}
+      >
+        {filteredGases.map((gas: GasReading) => createMarker(gas))}
+        {createHoverWindow(hoverMarker)}
+      </GoogleMap>
+    </>
   );
 };
 
-export default React.memo(GasesDotMap);
+export default React.memo(GasDotMap);
