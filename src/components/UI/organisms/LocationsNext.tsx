7cac89a98b1f083096af9e4652329e62ece8d29a
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-03-16 09:19:10 -0600

    Make filter bar float

--- src/components/UI/organisms/LocationsNext.tsx
+++ src/components/UI/organisms/LocationsNext.tsx
@@ -1,6 +1,18 @@
 import React, { useCallback, useState } from "react";
 import { TravelMap } from "../molecules/TravelMap";
 import { Filter, FilterBar } from "../molecules/FilterBar";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  filterBar: {
+    position: "fixed",
+    zIndex: "1",
+    width: "calc(100% - 48px - 240px)",
+  },
+  topMargin: {
+    height: "104px",
+  },
+});
 
 export const LocationsNext: React.FC = () => {
   const [filter, setFilter] = useState<Filter>({});
@@ -12,9 +24,28 @@ export const LocationsNext: React.FC = () => {
     []
   );
 
+  const styles = useStyles();
+
   return (
     <>
-      <FilterBar filter={filter} onChange={filterChange} />
+      <div className={styles.filterBar}>
+        <FilterBar filter={filter} onChange={filterChange} />
+      </div>
+      <div className={styles.topMargin} />
+      <div style={{ height: "600px" }}>
+        <TravelMap filter={filter} />
+      </div>
+      <div style={{ height: "16px" }} />
+      <div style={{ height: "600px" }}>
+        <TravelMap filter={filter} />
+      </div>
+      <div style={{ height: "16px" }} />
+
+      <div style={{ height: "600px" }}>
+        <TravelMap filter={filter} />
+      </div>
+      <div style={{ height: "16px" }} />
+
       <div style={{ height: "600px" }}>
         <TravelMap filter={filter} />
       </div>
