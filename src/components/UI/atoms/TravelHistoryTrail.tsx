9d08d1b934a85c170e4705314b20b9d857e94371
Melissa <57367085+melpicazo@users.noreply.github.com>
2022-03-15 21:36:56 -0600

    custom styling so that Backdrop doesn't overlay the whole screen

--- src/components/UI/atoms/TravelHistoryTrail.tsx
+++ src/components/UI/atoms/TravelHistoryTrail.tsx
@@ -3,14 +3,29 @@ import React, { useState, useEffect } from "react";
 import ControlPosition = google.maps.ControlPosition;
 import CircularProgress from "@mui/material/CircularProgress";
 import Backdrop from '@mui/material/Backdrop';
+import { makeStyles } from "@mui/styles";
 
 interface TravelHistoryTrailProps {
   center?: any;
   data: any;
 }
+
+const useStyles = makeStyles({
+  parent: {
+    position: "relative",
+    height: "575px",
+    zIndex: 0,
+  },
+  backdrop: {
+    position: "absolute",
+  },
+});
+
 export const TravelHistoryTrail: React.FC<TravelHistoryTrailProps> = (
   props
 ) => {
+  const styles = useStyles();
+
   const segments = props.data
     .map((person: any) => {
       return person.segments
@@ -35,88 +50,92 @@ export const TravelHistoryTrail: React.FC<TravelHistoryTrailProps> = (
 
   return (
     <>
-      <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={loading}
-      >
-        <CircularProgress size={200} />
-      </Backdrop>
-      <div
-        id={"travel-legend"}
-        hidden={!showLegend}
-        style={{
-          background: "rgb(255, 255, 255) none repeat scroll 0% 0% padding-box",
-          border: "0px none",
-          marginLeft: "10px",
-          padding: "0px 17px",
-          textTransform: "none",
-          appearance: "none",
-          cursor: "pointer",
-          userSelect: "none",
-          direction: "ltr",
-          overflow: "hidden",
-          verticalAlign: "middle",
-          color: "rgb(86, 86, 86)",
-          fontFamily: "Roboto, Arial, sans-serif",
-          fontSize: "18px",
-          borderBottomRightRadius: "2px",
-          borderTopRightRadius: "2px",
-          boxShadow: "rgba(0, 0, 0, 0.3) 0px 1px 4px -1px",
-        }}
-      >
-        <p>Legend</p>
-        {props.data.map((person: any) => (
-          // eslint-disable-next-line react/jsx-key
-          <div>
-            <p>
-              <span
-                style={{
-                  height: "16px",
-                  width: "16px",
-                  backgroundColor: person.color,
-                  borderRadius: "50%",
-                  display: "inline-block",
-                  marginRight: "8px",
-                }}
-              />
-              {person.name}
-            </p>
-          </div>
-        ))}
+      <div className={styles.parent}>
+        <Backdrop
+          className={styles.backdrop}
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={loading}
+        >
+          <CircularProgress size={200} />
+        </Backdrop>
+        <div
+          id={"travel-legend"}
+          hidden={!showLegend}
+          style={{
+            background:
+              "rgb(255, 255, 255) none repeat scroll 0% 0% padding-box",
+            border: "0px none",
+            marginLeft: "10px",
+            padding: "0px 17px",
+            textTransform: "none",
+            appearance: "none",
+            cursor: "pointer",
+            userSelect: "none",
+            direction: "ltr",
+            overflow: "hidden",
+            verticalAlign: "middle",
+            color: "rgb(86, 86, 86)",
+            fontFamily: "Roboto, Arial, sans-serif",
+            fontSize: "18px",
+            borderBottomRightRadius: "2px",
+            borderTopRightRadius: "2px",
+            boxShadow: "rgba(0, 0, 0, 0.3) 0px 1px 4px -1px",
+          }}
+        >
+          <p>Legend</p>
+          {props.data.map((person: any) => (
+            // eslint-disable-next-line react/jsx-key
+            <div>
+              <p>
+                <span
+                  style={{
+                    height: "16px",
+                    width: "16px",
+                    backgroundColor: person.color,
+                    borderRadius: "50%",
+                    display: "inline-block",
+                    marginRight: "8px",
+                  }}
+                />
+                {person.name}
+              </p>
+            </div>
+          ))}
+        </div>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={12}
+          center={center}
+          options={{ gestureHandling: "greedy" }}
+          onLoad={(map) => {
+            setLoading(true);
+            const controls = map.controls[ControlPosition.LEFT_TOP];
+            const legend = document.getElementById("travel-legend");
+            controls.push(legend);
+          }}
+          onTilesLoaded={() => {
+            setShowLegend(true);
+            setLoading(false);
+          }}
+        >
+          {segments.map((segment: any) => (
+            // eslint-disable-next-line react/jsx-key
+            <Polyline
+              path={segment.path}
+              options={{
+                strokeColor: segment.color,
+                strokeOpacity: 1,
+                strokeWeight: 3,
+                clickable: false,
+                draggable: false,
+                editable: false,
+                visible: true,
+                zIndex: 1,
+              }}
+            />
+          ))}
+        </GoogleMap>
       </div>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={12}
-        center={center}
-        options={{ gestureHandling: "greedy" }}
-        onLoad={(map) => {
-          setLoading(true);
-          const controls = map.controls[ControlPosition.LEFT_TOP];
-          const legend = document.getElementById("travel-legend");
-          controls.push(legend);
-        }}
-        onTilesLoaded={() => {
-          setShowLegend(true);
-          setLoading(false);
-        }}
-      >
-        {segments.map((segment: any) => (
-          // eslint-disable-next-line react/jsx-key
-          <Polyline
-            path={segment.path}
-            options={{
-              strokeColor: segment.color,
-              strokeOpacity: 1,
-              strokeWeight: 3,
-              clickable: false,
-              draggable: false,
-              editable: false,
-              visible: true,
-              zIndex: 1,
-            }}
-          />
-        ))}
-      </GoogleMap>
     </>
   );
 };
