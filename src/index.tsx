6e6d396e66186671fd7922b149c5d32399a73d02
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-03-16 01:50:16 -0600

    Replace old travel trail map

--- src/index.tsx
+++ src/index.tsx
@@ -11,29 +11,6 @@ export const API_URL = "https://func-api-nmisvbwuqreyq.azurewebsites.net";
 
 const CURRENT_USER_KEY = "current_user";
 
-export const PEOPLE_COLORS = [
-  "#e6194b",
-  "#3cb44b",
-  "#ffe119",
-  "#4363d8",
-  "#f58231",
-  "#911eb4",
-  "#46f0f0",
-  "#f032e6",
-  "#bcf60c",
-  "#fabebe",
-  "#008080",
-  "#e6beff",
-  "#9a6324",
-  "#fffac8",
-  "#800000",
-  "#aaffc3",
-  "#808000",
-  "#ffd8b1",
-  "#000075",
-  "#808080",
-];
-
 export const getCurrentUser = (): any | null => {
   const json = localStorage.getItem(CURRENT_USER_KEY);
   return json && JSON.parse(json);
