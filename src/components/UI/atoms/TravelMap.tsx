92feb79068acda780c6a3b6f7f4f61156c0f22aa
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-03-16 00:45:25 -0600

    Show legend

--- src/components/UI/atoms/TravelMap.tsx
+++ src/components/UI/atoms/TravelMap.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   CompanyLocationData,
   Person,
@@ -8,6 +8,8 @@ import {
 import { getCurrentUser } from "../../../index";
 import { GoogleMap, Polyline } from "@react-google-maps/api";
 import LatLngLiteral = google.maps.LatLngLiteral;
+import ControlPosition = google.maps.ControlPosition;
+import { v4 as uuidv4 } from "uuid";
 
 const TRAIL_SPLIT_MS = 10 * 60 * 1000;
 
@@ -37,6 +39,7 @@ interface Trail {
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface TravelMapProps {}
+
 export const TravelMap: React.FC<TravelMapProps> = (props) => {
   const user = getCurrentUser();
 
@@ -51,32 +54,90 @@ export const TravelMap: React.FC<TravelMapProps> = (props) => {
   const people: PersonWithLocationReadings[] = (data && intoPeople(data)) ?? [];
   const trails: Trail[] = intoTrails(people);
 
+  const [legendElementId] = useState(uuidv4().toString());
+  const [showLegend, setShowLegend] = useState(false);
+
   return (
-    <GoogleMap
-      mapContainerStyle={{
-        height: "100%",
-        width: "100%",
-      }}
-      zoom={12}
-      center={{
-        lat: 51.049999,
-        lng: -114.1283,
-      }}
-      options={{ gestureHandling: "greedy" }}
-    >
-      {trails.map((trail: any) => (
-        <Polyline
-          key={trailKey(trail)}
-          path={trail.path}
-          options={{
-            strokeColor: trail.color,
-            strokeOpacity: 1,
-            strokeWeight: 3,
-            clickable: false,
-          }}
-        />
-      ))}
-    </GoogleMap>
+    <>
+      <div
+        id={legendElementId}
+        hidden={!showLegend}
+        style={{
+          background: "rgb(255, 255, 255) none repeat scroll 0% 0% padding-box",
+          border: "0px none",
+          marginLeft: "10px",
+          padding: "0px 17px",
+          textTransform: "none",
+          appearance: "none",
+          cursor: "pointer",
+          userSelect: "none",
+          direction: "ltr",
+          overflow: "hidden",
+          verticalAlign: "middle",
+          color: "rgb(86, 86, 86)",
+          fontFamily: "Roboto, Arial, sans-serif",
+          fontSize: "18px",
+          borderBottomRightRadius: "2px",
+          borderTopRightRadius: "2px",
+          boxShadow: "rgba(0, 0, 0, 0.3) 0px 1px 4px -1px",
+        }}
+      >
+        <p>Legend</p>
+        {people.map((person, personIndex) => {
+          const color = indexToColor(personIndex);
+          return (
+            <div key={`${person.id}-${color}`}>
+              <p>
+                <span
+                  style={{
+                    height: "16px",
+                    width: "16px",
+                    backgroundColor: color,
+                    borderRadius: "50%",
+                    display: "inline-block",
+                    marginRight: "8px",
+                  }}
+                />
+                {person.name}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+      <GoogleMap
+        mapContainerStyle={{
+          height: "100%",
+          width: "100%",
+        }}
+        zoom={12}
+        center={{
+          lat: 51.049999,
+          lng: -114.1283,
+        }}
+        options={{ gestureHandling: "greedy" }}
+        onLoad={(map) => {
+          const controls = map.controls[ControlPosition.LEFT_TOP];
+          const legend = document.getElementById(legendElementId);
+          controls.push(legend);
+        }}
+        onTilesLoaded={() => {
+          setShowLegend(true);
+        }}
+      >
+        {trails.map((trail: any) => (
+          <Polyline
+            key={trailKey(trail)}
+            path={trail.path}
+            options={{
+              strokeColor: trail.color,
+              strokeOpacity: 1,
+              strokeWeight: 3,
+              clickable: false,
+            }}
+          />
+        ))}
+      </GoogleMap>
+    </>
   );
 };
 
