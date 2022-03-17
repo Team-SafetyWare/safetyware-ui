fa9cf73b0ed9024b3def4f5bc8e5c263aa08b5c0
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-03-16 22:10:25 -0600

    Improve tooltip styling

--- src/components/UI/molecules/IncidentsMap.tsx
+++ src/components/UI/molecules/IncidentsMap.tsx
@@ -41,9 +41,7 @@ const useStyles = makeStyles({
     position: "relative",
     right: "calc(100% / 2)",
     bottom: "216px",
-    filter: "drop-shadow(0 0 0.75rem crimson);",
-    // boxShadow:
-    //   "0px 3px 1px -2px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12)",
+    filter: "drop-shadow(0 0 5px rgba(0,0,0,0.2));",
   },
   tooltipContent: {
     position: "relative",
