0a161186004c35b06882f5d0e8d7a5a2931270c5
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-03-18 10:46:33 -0600

    Revert "Watch map type ID changes"
    This reverts commit 27dc896920c734fb5ca9b413d1a3d0dfffb39a3a.

--- src/components/UI/molecules/TravelMap.tsx
+++ src/components/UI/molecules/TravelMap.tsx
@@ -109,28 +109,8 @@ export const TravelMap: React.FC<TravelMapProps> = (props) => {
   const trails: Trail[] = intoTrails(people);
   const pointTree = intoPointTree(trails);
 
-  const [map, setMap] = useState<google.maps.Map | undefined>();
-
   const [legendElementId] = useState(uuidV4().toString());
   const [showLegend, setShowLegend] = useState(false);
-
-  const onMapLoad = useCallback((map: google.maps.Map) => {
-    setMap(map);
-    const controls = map.controls[ControlPosition.LEFT_TOP];
-    const legend = document.getElementById(legendElementId);
-    controls.push(legend);
-  }, []);
-
-  const onTilesLoaded = useCallback(() => {
-    setShowLegend(true);
-  }, []);
-
-  const [mapTypeId, setMapTypeId] = useState<string>();
-
-  const onMapTypeIdChanged = useCallback(() => {
-    const mapTypeId = map?.getMapTypeId();
-    mapTypeId && setMapTypeId(mapTypeId);
-  }, [map]);
 
   const [tooltipPoint, setTooltipPoint] = useState<QuadPoint | undefined>();
 
@@ -200,9 +180,14 @@ export const TravelMap: React.FC<TravelMapProps> = (props) => {
           options={{ gestureHandling: "greedy" }}
           zoom={DEFAULT_MAP_ZOOM}
           center={DEFAULT_MAP_CENTER}
-          onLoad={onMapLoad}
-          onTilesLoaded={onTilesLoaded}
-          onMapTypeIdChanged={onMapTypeIdChanged}
+          onLoad={(map) => {
+            const controls = map.controls[ControlPosition.LEFT_TOP];
+            const legend = document.getElementById(legendElementId);
+            controls.push(legend);
+          }}
+          onTilesLoaded={() => {
+            setShowLegend(true);
+          }}
         >
           {trails.map((trail: any) => (
             <Polyline
