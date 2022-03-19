2282f8779f690540389c6e8027c3ad1c06f6da2e
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-03-18 19:25:36 -0600

    Prefetch all data when filter parameters change

--- src/components/UI/molecules/Prefetch.tsx
+++ src/components/UI/molecules/Prefetch.tsx
@@ -1,6 +1,13 @@
 import React from "react";
-import { Filter } from "./FilterBar";
-import { useCompanyLocations } from "../../../util/queryService";
+import { Filter, shouldFilterPerson } from "./FilterBar";
+import {
+  useCompanyIncidents,
+  useCompanyIncidentStats,
+  useCompanyLocations,
+  usePersonIncidents,
+  usePersonIncidentStats,
+  usePersonLocations,
+} from "../../../util/queryService";
 import { getCurrentUser } from "../../../index";
 
 interface PrefetchProps {
@@ -10,10 +17,62 @@ interface PrefetchProps {
 export const Prefetch: React.FC<PrefetchProps> = (props) => {
   const user = getCurrentUser();
 
-  useCompanyLocations({
-    companyId: user?.company.id || "",
-    filter: props.filter,
-  });
+  usePersonLocations(
+    {
+      personId: props.filter.person?.id || "",
+      filter: {
+        minTimestamp: props.filter.minTimestamp,
+        maxTimestamp: props.filter.maxTimestamp,
+      },
+    },
+    shouldFilterPerson(props.filter)
+  );
+
+  useCompanyLocations(
+    {
+      companyId: user?.company.id || "",
+      filter: props.filter,
+    },
+    !shouldFilterPerson(props.filter)
+  );
+
+  usePersonIncidents(
+    {
+      personId: props.filter.person?.id || "",
+      filter: {
+        minTimestamp: props.filter.minTimestamp,
+        maxTimestamp: props.filter.maxTimestamp,
+      },
+    },
+    shouldFilterPerson(props.filter)
+  );
+
+  useCompanyIncidents(
+    {
+      companyId: user?.company.id || "",
+      filter: props.filter,
+    },
+    !shouldFilterPerson(props.filter)
+  );
+
+  usePersonIncidentStats(
+    {
+      personId: props.filter.person?.id || "",
+      filter: {
+        minTimestamp: props.filter.minTimestamp,
+        maxTimestamp: props.filter.maxTimestamp,
+      },
+    },
+    shouldFilterPerson(props.filter)
+  );
+
+  useCompanyIncidentStats(
+    {
+      companyId: user?.company.id || "",
+      filter: props.filter,
+    },
+    !shouldFilterPerson(props.filter)
+  );
 
   return <></>;
 };
