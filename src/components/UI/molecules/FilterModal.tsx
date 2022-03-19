dd17a75c743bf785fc892116cff85ada5aab0ab0
Owen Troke-Billard <owen.trokebillard@outlook.com>
2022-03-19 04:40:40 -0600

    Center modal

--- src/components/UI/molecules/FilterModal.tsx
+++ src/components/UI/molecules/FilterModal.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-import { Card, CardContent, Modal } from "@mui/material";
+import { Card, CardContent, Grid, Modal } from "@mui/material";
 import { Filter, FilterBar } from "./FilterBar";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles({
+  grid: {
+    width: "100%",
+    height: "100%",
+  },
+  card: {
+    margin: "16px",
+    width: "100%",
+    height: "fit-content",
+    maxWidth: "320px",
+  },
   filterBarContainer: {
     marginBottom: "-8px",
   },
@@ -22,18 +32,28 @@ export const FilterModal: React.FC<FilterModalProps> = (props) => {
 
   return (
     <Modal open={props.open} onClose={props.onClose}>
-      <Card elevation={2}>
-        <CardContent>
-          <div className={styles.filterBarContainer}>
-            <FilterBar
-              filter={props.filter}
-              onChange={props.onChange}
-              closeable={true}
-              onClose={props.onClose}
-            />
-          </div>
-        </CardContent>
-      </Card>
+      <Grid
+        className={styles.grid}
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid item>
+          <Card className={styles.card} elevation={2}>
+            <CardContent>
+              <div className={styles.filterBarContainer}>
+                <FilterBar
+                  filter={props.filter}
+                  onChange={props.onChange}
+                  closeable={true}
+                  onClose={props.onClose}
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </Modal>
   );
 };
