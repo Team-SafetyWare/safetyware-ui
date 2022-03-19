ff843d206d55446d019fd3f098f242ce7ed06730
nmoton <26640786+nmoton@users.noreply.github.com>
2022-03-18 22:00:36 -0600

    SAF-287: Remove filtering button for each dashboard widget

--- src/components/UI/atoms/DashboardWidgetTile.tsx
+++ src/components/UI/atoms/DashboardWidgetTile.tsx
@@ -54,13 +54,7 @@ const useStyles = makeStyles({
     fontSize: "24px",
     margin: "12px 0px 12px 8px",
   },
-  widgetButtons: {
-    display: "flex",
-  },
-  removeButton: {
-    marginRight: "16px",
-  },
-  filterButton: {},
+  removeButton: {},
   widget: {
     height: "100%",
     width: "100%",
@@ -78,24 +72,14 @@ export const DashboardWidgetTile: React.FC<DashboardSummaryTileProps> = (
       <div className={styles.widgetTile}>
         <div className={styles.widgetInfo}>
           <p className={styles.widgetName}>{props.widget.widgetName}</p>
-          <div className={styles.widgetButtons}>
-            <div className={styles.removeButton}>
-              <BootstrapButton
-                variant="outlined"
-                endIcon={<RemoveIcon />}
-                onClick={() => props.removeWidget(props.widget)}
-              >
-                Remove Widget
-              </BootstrapButton>
-            </div>
-            <div className={styles.filterButton}>
-              <BootstrapButton
-                variant="outlined"
-                endIcon={<FilterAltOutlinedIcon />}
-              >
-                Filter
-              </BootstrapButton>
-            </div>
+          <div className={styles.removeButton}>
+            <BootstrapButton
+              variant="outlined"
+              endIcon={<RemoveIcon />}
+              onClick={() => props.removeWidget(props.widget)}
+            >
+              Remove Widget
+            </BootstrapButton>
           </div>
         </div>
         <div className={styles.widget}>{props.widget.widget}</div>
